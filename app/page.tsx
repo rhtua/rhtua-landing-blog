e1b7778ba75448b20fd862316a2e6b543a76fab5
Arthur Fusieger <artoliveira13@gmail.com>
2025-06-09 02:57:09 -0300

    add observability

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import client from '@/tina/__generated__/client';
 import Layout from '@/components/layout/layout';
 import ClientPage from './[...urlSegments]/client-page';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { BasePage } from '@/landing/components';
 
 export const revalidate = 300;
@@ -14,6 +16,8 @@ export default async function Home() {
 
   return (
     <>
+      <Analytics />
+      <SpeedInsights />
       {isHome && <BasePage />}
       {!isHome && (
         <Layout rawPageData={data}>
